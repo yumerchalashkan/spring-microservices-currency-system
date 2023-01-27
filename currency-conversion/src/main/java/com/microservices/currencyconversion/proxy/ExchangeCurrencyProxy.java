a9a378b3fd1e4877f9ddbcd42b2eafc5b9ca9f14
Yumer Chalashkan <71348719+ocaliskan@users.noreply.github.com>
2023-01-27 16:44:29 +0300

    added eureka server and load balancer

--- currency-conversion/src/main/java/com/microservices/currencyconversion/proxy/ExchangeCurrencyProxy.java
+++ currency-conversion/src/main/java/com/microservices/currencyconversion/proxy/ExchangeCurrencyProxy.java
@@ -5,7 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "exchange-currency",url = "http://localhost:8080")
+
+//@FeignClient(name = "exchange-currency",url = "http://localhost:8080")
+@FeignClient(name = "currency-service")
 public interface ExchangeCurrencyProxy {
     @GetMapping("/currency/from/{from}/to/{to}")
     public ConversionCurrency getCurrencyAmount(@PathVariable String from, @PathVariable String to);
---
+++ naming-server-eureka/src/test/java/com/example/namingservereureka/NamingServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.namingservereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
